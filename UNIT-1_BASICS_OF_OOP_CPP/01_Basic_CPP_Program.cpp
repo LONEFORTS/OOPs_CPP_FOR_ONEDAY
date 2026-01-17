e0df931a6b7d06228d5526e7a348a9b77e2fd5fa
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-17 17:27:41 +0530

    Update 01_Basic_CPP_Program.cpp

--- UNIT-1_BASICS_OF_OOP_CPP/01_Basic_CPP_Program.cpp
+++ UNIT-1_BASICS_OF_OOP_CPP/01_Basic_CPP_Program.cpp
@@ -1,1 +1,18 @@
+// Program to demonstrate basic input and output in C++
+// Unit-I: Basics of OOP and C++
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[20];
+
+    cout<<"Enter Your Name: ";
+    cin.getline(name, 20); //Here the getiline() reads the whole string including spaces
+    
+
+    cout << "Your Name is: " << name << endl;
+
+    return 0;
+}
